985a12c8bff1934c530e718ae4b82823b79a0a77
Alexcmyk <alex.kirts@gmail.com>
2021-02-14 12:52:26 -0700

    adds nav component

--- js/app.js
+++ js/app.js
@@ -1,41 +1,9 @@
-const Highlight = (props) => (
-  <span className={`relative highlight highlight-${props.color}`}>
+const Highlight = ({ color, children }) => (
+  <span className={`relative highlight highlight-${color}`}>
     {/* Whole template literal needs to be wrapped in brackets */}
-    <span className="relative z-2">{props.children}</span>
+    <span className="relative z-2">{children}</span>
     {/* props.children  refers to whatever content is inside a component*/}
   </span>
-);
-
-const Nav = () => (
-  <nav className="pt3 pt4-ns mb4 mb0-ns">
-    <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">
-      <li className="mh2-ns f6 f4-l tc order-3-ns w-100 w-30-ns mb3 mb0-ns">
-        <a href="index.html" className="white no-underline">
-          <img src="../images/logo.svg" className="db center logo" alt="" />
-        </a>
-      </li>
-      <li className="mh2-ns f6 f4-l tc order-1-ns w-20">
-        <a href="#" className="white no-underline">
-          About
-        </a>
-      </li>
-      <li className="mh2-ns f6 f4-l tc order-2-ns w-20">
-        <a href="#" className="white no-underline">
-          Tickets
-        </a>
-      </li>
-      <li className="mh2-ns f6 f4-l tc order-4-ns w-20">
-        <a href="#" className="white no-underline">
-          Journal
-        </a>
-      </li>
-      <li className="mh2-ns f6 f4-l tc order-5-ns w-20">
-        <a href="#" className="white no-underline">
-          Contact
-        </a>
-      </li>
-    </ul>
-  </nav>
 );
 
 const Intro = () => (
@@ -51,6 +19,30 @@ const Intro = () => (
       giving. <Highlight color="yellow">Dattebayo!</Highlight>
     </div>
   </div>
+);
+
+// Spread Operator sends all items to the props argument and passes them where its needed. | We destructured the "props" down to just the key items we needed
+const NavItem = ({ className, href, children, logo }) => (
+  <li className={`mh2-ns f6 f4-1 tc ${className}`}>
+    {/* Each unique class name */}
+    <a className="white no-underline" href={href}>
+      {/* Each unique href*/}
+      {logo ? <img src="../images/logo.svg"></img> : children}
+      {/* Each unique "child" i.e innerText */}
+    </a>
+  </li>
+);
+
+const Nav = () => (
+  // Adding classes to the nav and ul items
+  <nav className="pt3 pt4-ns mb4 mb0-ns">
+    <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">
+      {/* Using map to iterate over each menu item in menu.js and applying JS code to interject the appropriate object item */}
+      {menu.map((item) => (
+        <NavItem {...item} />
+      ))}
+    </ul>
+  </nav>
 );
 
 const App = () => (
